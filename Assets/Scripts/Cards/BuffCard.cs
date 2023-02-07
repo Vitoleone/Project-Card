ce67ef87c82c0c0fcfc18b3e9e873b91495f8408
Noyan Berkay Aşkın <74705281+Vitoleone@users.noreply.github.com>
2023-02-07 19:05:14 +0300

    Other cards scripts added

---
+++ Assets/Scripts/Cards/BuffCard.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class BuffCard : MonoBehaviour
+{
+    private Vector3 defaultScale;
+    private void Start()
+    {
+        Physics2D.queriesHitTriggers = true;
+        defaultScale = transform.localScale;
+    }
+
+    private void OnMouseDown()
+    {
+        Debug.Log("Buff");
+    }
+
+    private void OnMouseEnter()
+    {
+        transform.DOScale(defaultScale*1.2f, 0.3f);
+    }
+    private void OnMouseExit()
+    {
+        transform.DOScale(defaultScale, 0.3f);
+    }
+}
---
+++ Assets/Scripts/Cards/HeroCard.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class HeroCard : MonoBehaviour
+{
+    private Vector3 defaultScale;
+    private void Start()
+    {
+        Physics2D.queriesHitTriggers = true;
+        defaultScale = transform.localScale;
+    }
+
+    private void OnMouseDown()
+    {
+        Debug.Log("Hero");
+    }
+
+    private void OnMouseEnter()
+    {
+        transform.DOScale(defaultScale*1.2f, 0.3f);
+    }
+    private void OnMouseExit()
+    {
+        transform.DOScale(defaultScale, 0.3f);
+    }
+}
