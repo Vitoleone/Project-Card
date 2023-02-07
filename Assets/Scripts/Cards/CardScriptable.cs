27a9192ce26f68298e25c3d584d5c492a12531a8
Noyan Berkay Aşkın <74705281+Vitoleone@users.noreply.github.com>
2023-02-07 16:50:44 +0300

    CardScriptable.cs added
    This script is a constructor script of all cards

---
+++ Assets/Scripts/Cards/CardScriptable.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+[CreateAssetMenu(fileName = "Card",menuName = "ScriptableObject/Card")]
+public class CardScriptable : ScriptableObject
+{
+    public string cardName;
+    public double cardMana;
+    public string cardDescription;
+    public CardType cardType;
+    public Image cardImage;
+    public UnityEvent cardEvent;
+    
+    
+    public enum CardType
+    {
+        Attack,
+        Defence,
+        Buff,
+        Hero
+    }
+    
+}
