0418205849cf104bda7465a2799b1284abbc553d
Noyan Berkay Aşkın <74705281+Vitoleone@users.noreply.github.com>
2023-02-07 18:16:36 +0300

    cards have tween effect

--- Assets/Scripts/Cards/AttackCard.cs
+++ Assets/Scripts/Cards/AttackCard.cs
@@ -2,16 +2,28 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class AttackCard : MonoBehaviour
 {
+  private Vector3 defaultScale;
   private void Start()
   {
     Physics2D.queriesHitTriggers = true;
+    defaultScale = transform.localScale;
   }
 
   private void OnMouseDown()
   {
     Debug.Log("Attack");
+  }
+
+  private void OnMouseEnter()
+  {
+    transform.DOScale(defaultScale*1.2f, 0.3f);
+  }
+  private void OnMouseExit()
+  {
+    transform.DOScale(defaultScale, 0.3f);
   }
 }
