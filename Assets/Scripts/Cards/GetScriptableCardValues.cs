5677c056b0b353de93826319e22849687f20338d
Noyan Berkay Aşkın <74705281+Vitoleone@users.noreply.github.com>
2023-02-11 17:42:13 +0300

    Card assets added and card prefab changes

--- Assets/Scripts/Cards/GetScriptableCardValues.cs
+++ Assets/Scripts/Cards/GetScriptableCardValues.cs
@@ -7,18 +7,39 @@ using UnityEngine.UI;
 public class GetScriptableCardValues : MonoBehaviour
 {
     public Image sprite;
+    public Image typeTemplate;
+    public Sprite attackTemplate;
+    public Sprite buffTemplate;
+    public Sprite deffenceTemplate;
+    public Sprite heroTemplate;
     public TextMeshProUGUI descriptionText;
     public TextMeshProUGUI cardName;
     public TextMeshProUGUI manaText;
-    public TextMeshProUGUI cardType;
+
+
     public CardScriptable cardScriptable;
     void Start()
     {
         sprite.sprite = cardScriptable.cardImage;
         descriptionText.text = cardScriptable.cardDescription;
         cardName.text = cardScriptable.cardName;
-        cardType.text = cardScriptable.cardType.ToString();
         manaText.text = cardScriptable.cardMana.ToString();
+        switch (cardScriptable.cardType)
+        {
+            case CardScriptable.CardType.Attack:
+                typeTemplate.sprite = attackTemplate;
+                break;
+            case CardScriptable.CardType.Buff:
+                typeTemplate.sprite = buffTemplate;
+                break;
+            case CardScriptable.CardType.Defence:
+                typeTemplate.sprite = deffenceTemplate;
+                break;
+            case CardScriptable.CardType.Hero:
+                typeTemplate.sprite = heroTemplate;
+                break;
+            
+        }
         
     }
     public enum CardType
