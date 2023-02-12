03ecc660e8915b36c93848b0cd010a44a50ea3d5
Noyan Berkay Aşkın <74705281+Vitoleone@users.noreply.github.com>
2023-02-12 20:36:30 +0300

    Cards movement adjustment cards now moving with tween

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using Unity.Mathematics;
 using Unity.VisualScripting;
 using UnityEngine;
@@ -23,17 +24,25 @@ public class GameManager : MonoBehaviour
     private void Start()
     {
         copyDeck = deck.cards;
-        DrawCard();
+    }
+
+    private void Update()
+    {
+        if (hand.Count <= 0)
+        {
+            DrawCard();
+        }
     }
 
     public void DrawCard()
     {
-        
+        if(hand.Count <= 0)//if player has no cards on hand or on the player turn draw cards
         for (int i = 0; i < maxCardNumber; i++)
         {
             int randomNumber = Random.Range(0, copyDeck.Count-1);
             cardOnHand.GetComponent<GetScriptableCardValues>().UpdateValues(copyDeck[randomNumber]);
-            GameObject newHandCard = Instantiate(cardOnHand, new Vector3(i * 7 - 17, -6, i), quaternion.identity);
+            GameObject newHandCard = Instantiate(cardOnHand, transform.position, quaternion.identity);
+            newHandCard.transform.DOMove(new Vector3(i * 7 - 17, -6, i), 1f).OnUpdate(()=>newHandCard.SetActive(true)).SetDelay(0.75f);
             AddCardFunction(newHandCard);
             hand.Add(newHandCard);
             
